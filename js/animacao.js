4b312ad5df6bc960b3285624276a17aefdc9bfa8
Klinsmann N.Lucas <multiklinsmann@gmail.com>
2017-08-26 20:09:12 -0300

    ADD img

--- js/animacao.js
+++ js/animacao.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.nav-link, #btn-inicio').click(function(e){
+  $('.nav-link, #btn-inicio, .footer-link').click(function(e){
     var linkHref = $(this).attr("href");
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top
