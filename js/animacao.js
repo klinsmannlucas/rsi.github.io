1fd2e12e9116d7e81ec7573d462d57c962efd457
Klinsmann N.Lucas <multiklinsmann@gmail.com>
2017-08-26 17:25:33 -0300

    Animação ADD + Botão

---
+++ js/animacao.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.nav-link, #btn-inicio').click(function(e){
+    var linkHref = $(this).attr("href");
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    }, 700);
+    e.preventDefault();
+  });
+});
