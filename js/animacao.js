f823061ec7b31feb015f115d069c30956fe89933
klinsmannlucas <multiklinsmann@gmail.com>
2017-08-31 12:34:45 -0300

    Animação de hide do navbar

--- js/animacao.js
+++ js/animacao.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.nav-link, #btn-inicio, .footer-link').click(function(e){
+    $('collapse').collapse('hide');
     var linkHref = $(this).attr("href");
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top
